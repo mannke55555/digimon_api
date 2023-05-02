e47af9cf9cd565a8b9029839892da56aeb3071cc
mannke55555 <ricardo@ignacioletelier.com>
2023-05-02 15:43:26 -0400

    actualización

--- assets/js/digimon.js
+++ assets/js/digimon.js
@@ -1,14 +1,27 @@
-fetch('https://digimon-api.vercel.app/api/digimon')
-  .then(response => response.json())
-  .then(data => {
-    const digimonData = document.getElementById('digimon-data');
-    data.forEach(digimon => {
-      const name = digimon.name;
-      const nameElement = document.createElement('p');
-      nameElement.textContent = name;
-      digimonData.appendChild(nameElement);
-    });
-  })
-  .catch(error => {
-    console.error('Error al obtener los datos', error);
-  });
+var url = "https://digimon-api.vercel.app/api/digimon"
+
+var contenido = document.querySelector("#contenido")
+
+fetch(url)
+.then(response => response.json())
+.then(datos => {
+    console.log(datos)
+
+    for (item of datos){
+
+    contenido.innerHTML += `
+    <div class="tarjeta">
+        <div class="card" style="width: 18rem;">
+        <img src="${item.img}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${item.name}</h5>
+          <p class="card-text">${item.level}</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+        </div>
+    </div>    
+    `
+
+}
+
+})
